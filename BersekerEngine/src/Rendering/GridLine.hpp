5968cc2bee4fd752546f6ad3c9bde995fbb9bc05
Vatamanu Bogdan <vatamanu.bogdan41@gmail.com>
2022-06-01 21:22:01 +0300

    Shaders: Added ShaderRegistry class for shader storing and also added shader_resouces_generator.py for shader indexing

--- BersekerEngine/src/Rendering/GridLine.hpp
+++ BersekerEngine/src/Rendering/GridLine.hpp
@@ -3,27 +3,23 @@
 #include "Rendering/GraphicsAPI/VertexArray.hpp"
 #include "Rendering/GraphicsAPI/VertexBuffer.hpp"
 #include "Rendering/GraphicsAPI/IndexBuffer.hpp"
+#include "ResourceLoaders/ShaderRegistry.h"
 
 class GridLine {
 public:
 	GridLine()
-		: shader(LoadShader()) {
+		: shader(ShaderRegistry::Get().GetShader(ShaderResource::GridLine)) {
 	}
 
 	void Draw(const glm::mat4 &view, const glm::mat4 &projection) {
-		shader.Bind();
-		shader.SetUniform("Projection", projection);
-		shader.SetUniform("View", view);
+		shader->Bind();
+		shader->SetUniform("Projection", projection);
+		shader->SetUniform("View", view);
 		vao.Bind();
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 	}
 
 private:
-	static ShaderProgram LoadShader() {
-		return ShaderProgram::LoadFrom("res/shaders/GridLine.vert.glsl", "res/shaders/GridLine.frag.glsl");
-	}
-
-private:
 	VertexArray vao;
-	ShaderProgram shader;
+	std::shared_ptr<ShaderProgram> shader;
 };
---
+++ BersekerEngine/src/ResourceLoaders/ShaderResources.cpp
@@ -0,0 +1,17 @@
+#include "ShaderResources.h"
+
+#include <type_traits>
+
+ShaderSourceFiles ShaderResources::shaderSourceFiles[] = {
+	ShaderSourceFiles("res/shaders/Model3D.vert.glsl", "res/shaders/Model3D.frag.glsl"),
+	ShaderSourceFiles("res/shaders/Primitive.vert.glsl", "res/shaders/Primitive.frag.glsl"),
+	ShaderSourceFiles("res/shaders/LightedModel.vert.glsl", "res/shaders/LightedModel.frag.glsl"),
+	ShaderSourceFiles("res/shaders/LightSource.vert.glsl", "res/shaders/LightSource.frag.glsl"),
+	ShaderSourceFiles("res/shaders/GridLine.vert.glsl", "res/shaders/GridLine.frag.glsl"),
+
+};
+
+const ShaderSourceFiles& ShaderResources::Get(ShaderResource identifier) {
+	auto index = static_cast<std::underlying_type<ShaderResource>::type>(identifier);
+	return shaderSourceFiles[index];
+}
