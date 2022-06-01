e297754a57580aa319214f3dbc00180cd7a4c796
Vatamanu Bogdan <vatamanu.bogdan41@gmail.com>
2022-06-01 12:53:51 +0300

    Lighting: Added material panel and applied material prorieties to lighted entity

--- BersekerEngine/src/ECS/Components/Material.hpp
+++ BersekerEngine/src/ECS/Components/Material.hpp
@@ -8,13 +8,15 @@ public:
 	glm::vec3					Ambient;
 	glm::vec3					Diffuse;
 	glm::vec3 					Specular;
+	float						Shininess;
 	std::shared_ptr<ShaderProgram> 	Shader = nullptr;
 
 	Material(const glm::vec3 &ambient,
 		   const glm::vec3 &diffuse,
 		   const glm::vec3 &specular,
+		   float shininess,
 		   const std::shared_ptr<ShaderProgram> &shaderProgram)
-		   : Ambient(ambient), Diffuse(diffuse), Specular(specular), Shader(shaderProgram) {
+		   : Ambient(ambient), Diffuse(diffuse), Specular(specular), Shininess(shininess), Shader(shaderProgram) {
 	}
 
 	bool IsLighted() { return mask[0]; }
