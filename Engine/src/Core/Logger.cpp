5942df64325ca172d0bdeb17f999e17515f477ce
Vatamanu Bogdan <vatamanu.bogdan41@gmail.com>
2022-09-08 18:16:30 +0300

    Core: Modified logger

--- Engine/src/Core/Logger.cpp
+++ Engine/src/Core/Logger.cpp
@@ -1,1 +1,39 @@
 #include "Logger.h"
+
+
+std::unique_ptr<Logger> Logger::logger = std::make_unique<DefaultLogger>();
+
+void DefaultLogger::LogDebug(const char *message) {
+	spdlog::debug(message);
+}
+
+void DefaultLogger::LogInfo(const char *message) {
+	spdlog::info(message);
+}
+
+void DefaultLogger::LogWarning(const char *message) {
+	spdlog::warn(message);
+}
+
+void DefaultLogger::LogError(const char *message) {
+	spdlog::error(message);
+}
+
+void DefaultLogger::SetLevel(Logger::Level level) {
+	spdlog::level::level_enum spdlogLevel;
+	switch (level) {
+		case DEBUG:
+			spdlogLevel = spdlog::level::debug;
+			break;
+		case INFO:
+			spdlogLevel = spdlog::level::info;
+			break;
+		case WARNING:
+			spdlogLevel = spdlog::level::warn;
+			break;
+		case ERROR:
+			spdlogLevel = spdlog::level::err;
+			break;
+	}
+	spdlog::set_level(spdlogLevel);
+}
--- Engine/src/Core/Logger.h
+++ Engine/src/Core/Logger.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <spdlog/spdlog.h>
 
+
+class DefaultLogger;
+
 class Logger {
 public:
 	enum Level {
@@ -9,6 +12,13 @@ public:
 		WARNING,
 		ERROR
 	};
+
+	virtual void LogDebug(const char* message) = 0;
+	virtual void LogInfo(const char* message) = 0;
+	virtual void LogWarning(const char* message) = 0;
+	virtual void LogError(const char* message) = 0;
+
+	virtual void SetLevel(Level level) = 0;
 
 public:
 	template <Level level, typename... Args>
@@ -25,23 +35,21 @@ public:
 	}
 
 	static void SetLoggerLevel(Level level) {
-		spdlog::level::level_enum spdlogLevel;
-		switch (level) {
-			case DEBUG:
-				spdlogLevel = spdlog::level::debug;
-				break;
-			case INFO:
-				spdlogLevel = spdlog::level::info;
-				break;
-			case WARNING:
-				spdlogLevel = spdlog::level::warn;
-				break;
-			case ERROR:
-				spdlogLevel = spdlog::level::err;
-				break;
-		}
-		spdlog::set_level(spdlogLevel);
+		logger->SetLevel(level);
 	}
+
+private:
+	static std::unique_ptr<Logger> logger;
+};
+
+class DefaultLogger final : public Logger {
+public:
+	void LogDebug(const char* message) override;
+	void LogInfo(const char* message) override;
+	void LogWarning(const char* message) override;
+	void LogError(const char* message) override;
+
+	void SetLevel(Level level) override;
 };
 
 
